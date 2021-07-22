0873ff08561a729fa8f5ff34592de88ecedf5ac3
Harry Potter <hp@mail.com>
2021-07-22 05:47:15 -0400

    File System

--- .git.py
+++ .git.py
@@ -1,18 +1,62 @@
-from github3 import login, GitHub
+from github3 import login
 import sys
 import base64
+import uuid
 
 
-config = dict()
 
 
-with open('config/git', 'r') as git_file:
-	output = git_file.read().split('\n')
-	config['git_username'] = base64.b64decode(output[0]).decode()
-	config['git_pass'] = base64.b64decode(output[1]).decode()
-	config['git_repo'] = base64.b64decode(output[2]).decode()
-	del output
+class Git:
+	def __init__(self):
+		self.config = dict()
+		with open('config/git.cfg', 'r') as git_file:
+			output = git_file.read().split('\n')
+			self.config['git_username'] = base64.b64decode(output[0]).decode()
+			self.config['git_pass'] = base64.b64decode(output[1]).decode()
+			self.config['git_repo'] = base64.b64decode(output[2]).decode()
+			del output
 
 
-try:
-	git_client = login(config['git_username'], config['git_pass'])
+		try:
+			self.git_client = login(self.config['git_username'], self.config['git_pass'])
+			self.git_repo = self.git_client.repository('ron1n8', 'society')
+			self.git_branch = self.git_repo.branch('master')
+			self.git_repo_content = self.getRepoContent()
+			
+		except Exception as e:
+			print('Error >> ', e)
+
+
+
+
+	def getRepoContent(self):
+
+		tree = self.git_branch.commit.commit.tree.to_tree().recurse()
+		files = list()
+		raw_files = list()
+
+		for file in tree.tree:
+			raw_files.append(file)
+			files.append(file.path)
+
+
+		return {'plain': files, 'raw': raw_files}
+
+
+	def getPlainFileContent(self, file):
+		return base64.b64decode(self.git_repo.blob(file._json_data['sha']).content)
+
+
+
+	def updateLocalFiles(self):
+		...
+
+
+	def sendData(self):
+		...
+
+
+
+if __name__ == '__main__':
+	# git_object = Git()
+	print(uuid.uuid4().hex)
